85cfff228be10ba30ade2ed30a796d8b3ada3729
Erwan Jouan <erwan.jouan@theatomicity.com>
2026-06-30 08:15:45 +0200

    downgrade to java17

--- src/main/java/com/theatomicity/aop/ut/generator/config/UtGeneratorCondition.java
+++ src/main/java/com/theatomicity/aop/ut/generator/config/UtGeneratorCondition.java
@@ -4,20 +4,12 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
 
-import java.util.Arrays;
-
 
 public class UtGeneratorCondition implements Condition {
-
-    public static final String UT_GENERATOR_PROFILE = "ut-generator";
 
     @Override
     public boolean matches(final ConditionContext context, final AnnotatedTypeMetadata metadata) {
         final String firstPackage = context.getEnvironment().getProperty("ut-generator.packages[0]");
-        if (firstPackage == null || firstPackage.isBlank()) {
-            return false;
-        }
-        return Arrays.stream(context.getEnvironment().getActiveProfiles())
-                .anyMatch(p -> p.equals(UT_GENERATOR_PROFILE));
+        return firstPackage != null && !firstPackage.isBlank();
     }
 }
